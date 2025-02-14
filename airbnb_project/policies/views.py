4209351d0e384f8be95ecb5a201f6439a84d6fe5
Vimanyu Aggarwal <vimanyu1022007@gmail.com>
2025-02-13 19:12:36 -0800

    chore: wip policy evaluation

--- airbnb_project/policies/views.py
+++ airbnb_project/policies/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+import logging
 
-# Create your views here.
+from listings.listing_models import Listing
+from policies.services.business_licence_client import BusinessLicenceClient
+
+# Set up logger for this module
+logger = logging.getLogger(__name__)
+
+
+@require_http_methods(["GET"])
+def evaluate_policies(request):
+    """
+       Django view to initiate the policy evaluation
+       """
+    try:
+        logger.info("Processing Listings....")
+        print('xxxxxxxxxxxxx')
+        for listing in Listing.objects.all():
+            print(listing)
+            business_licences_number = listing.registration_number
+            status = BusinessLicenceClient().get_licence_status(business_licences_number)
+            print(status)
+            print('--------------')
+        return HttpResponse("Evaluating Policies started 2", status=202)
+    except Exception as e:
+        # Log any unexpected errors
+        logger.error(f"Failed to start policy evaluation process: {str(e)}")
+        return HttpResponse("Failed to start policy evaluation process", status=500)
